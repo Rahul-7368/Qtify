2f664d7cde6330ec16e4f09c54596dda42fc35ef
RAHUL KUMAR RANJAN <152094372+Rahul-7368@users.noreply.github.com>
2024-05-30 11:31:28 +0530

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,30 @@
+import React from "react";
 import logo from "../../assests/logo.png";
 import styles from "./Logo.module.css";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const Logo = () => {
-  const navigate = useNavigate();
-  return (
-    <img
-      onClick={() => navigate("/")}
-      src={logo}
-      alt="logo"
-      className={styles.logo}
-    />
-  );
-};
+//   const navigate = useNavigate();
+//   return (
+//     <img
+//       onClick={() => navigate("/")}
+//       src={logo}
+//       alt="logo"
+//       className={styles.logo}
+//     />
+//   );
+// };
+
+  const Logo = () => {
+  return(
+    <div class="img-container">
+    <img src={logo} width={67} alt="logo not available" />
+  
+    </div>
+  )
+
+  // return <img src={logo} width={67} alt="logo not available" />;
+}
 
 export default Logo;
+
