6ee82be2c12c6e985de6c2f2c277b355e1dc2221
RAHUL KUMAR RANJAN <152094372+Rahul-7368@users.noreply.github.com>
2024-05-30 11:48:21 +0530

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,17 +3,9 @@ import logo from "../../assests/logo.png";
 import styles from "./Logo.module.css";
 // import { useNavigate } from "react-router-dom";
 
-const Logo = () => {
-//   const navigate = useNavigate();
-//   return (
-//     <img
-//       onClick={() => navigate("/")}
-//       src={logo}
-//       alt="logo"
-//       className={styles.logo}
-//     />
-//   );
-// };
+
+
+
 
   const Logo = () => {
   return(
